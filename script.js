06324c499ba10d478b387a5de4b43231d4767c4d
Megan <megisalia@gmail.com>
2023-05-31 00:42:31 +0400

    loops

---
+++ script.js
@@ -0,0 +1,41 @@
+// __________1st
+// for(let i=1; i < 20; i++){
+//     if(i % 2 === 0 ) console.log(i);  
+// }
+
+
+
+// ____________2nd
+
+// let count = 0;
+
+// for(let i = 1; i < 120; i++){
+//     if (i % 4 === 0) count++;
+// }
+
+// console.log(count);
+
+
+
+// _____________3rd
+
+// let number = +prompt("Enter the number: ");
+// let count = 2;
+
+// for (let i = 2; i < number; i++){
+//     if (number % i === 0) count++;
+// }
+// console.log(count);
+
+
+
+// ________________4th
+
+// do {
+//     var number = +prompt("Enter the number 1: ");
+//     var secondNumber = +prompt("Enter the number 2: ");
+// } while (number - secondNumber === 1 || secondNumber - number === 1);
+
+// for (let i = number + 1; i < secondNumber; i++){
+//     console.log(i);
+// }
